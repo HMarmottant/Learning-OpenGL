9bcf57e0fe0a4c00742a72df48a61e5c843f5206
DansTaMouette <64147327+DansTaMouette@users.noreply.github.com>
2021-12-17 21:23:02 +0100

    ErrorMessageCallback

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -5,11 +5,137 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <time.h>
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
 	x;\
 	ASSERT(GLLogCall())
+
+#pragma region glDebugMessageCallback
+// This is free and unencumbered software released into the public domain.
+// 
+// Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+// this software, either in source code form or as a compiled binary, for any
+// purpose, commercial or non-commercial, and by any means.
+// 
+// In jurisdictions that recognize copyright laws, the author or authors of this
+// software dedicate any and all copyright interest in the software to the
+// public domain. We make this dedication for the benefit of the public at large
+// and to the detriment of our heirs and successors. We intend this dedication
+// to be an overt act of relinquishment in perpetuity of all present and future
+// rights to this software under copyright law.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <http://unlicense.org/>
+
+// REQUIREMENTS: OpenGL version with the KHR_debug extension available.
+
+// Callback function for printing debug statements
+void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
+	GLenum severity, GLsizei length,
+	const GLchar* msg, const void* data)
+{
+	std::string _source;
+	std::string _type;
+	std::string _severity;
+
+	switch (source) {
+	case GL_DEBUG_SOURCE_API:
+		_source = "API";
+		break;
+
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+		_source = "WINDOW SYSTEM";
+		break;
+
+	case GL_DEBUG_SOURCE_SHADER_COMPILER:
+		_source = "SHADER COMPILER";
+		break;
+
+	case GL_DEBUG_SOURCE_THIRD_PARTY:
+		_source = "THIRD PARTY";
+		break;
+
+	case GL_DEBUG_SOURCE_APPLICATION:
+		_source = "APPLICATION";
+		break;
+
+	case GL_DEBUG_SOURCE_OTHER:
+		_source = "UNKNOWN";
+		break;
+
+	default:
+		_source = "UNKNOWN";
+		break;
+	}
+
+	switch (type) {
+	case GL_DEBUG_TYPE_ERROR:
+		_type = "ERROR";
+		break;
+
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+		_type = "DEPRECATED BEHAVIOR";
+		break;
+
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+		_type = "UDEFINED BEHAVIOR";
+		break;
+
+	case GL_DEBUG_TYPE_PORTABILITY:
+		_type = "PORTABILITY";
+		break;
+
+	case GL_DEBUG_TYPE_PERFORMANCE:
+		_type = "PERFORMANCE";
+		break;
+
+	case GL_DEBUG_TYPE_OTHER:
+		_type = "OTHER";
+		break;
+
+	case GL_DEBUG_TYPE_MARKER:
+		_type = "MARKER";
+		break;
+
+	default:
+		_type = "UNKNOWN";
+		break;
+	}
+
+	switch (severity) {
+	case GL_DEBUG_SEVERITY_HIGH:
+		_severity = "HIGH";
+		break;
+
+	case GL_DEBUG_SEVERITY_MEDIUM:
+		_severity = "MEDIUM";
+		break;
+
+	case GL_DEBUG_SEVERITY_LOW:
+		_severity = "LOW";
+		break;
+
+	case GL_DEBUG_SEVERITY_NOTIFICATION:
+		_severity = "NOTIFICATION";
+		break;
+
+	default:
+		_severity = "UNKNOWN";
+		break;
+	}
+
+	std::cout << id << " : " << _type << " of " << _severity << " severity, raised from " << _source << " : " << msg << std::endl;
+	if (_severity == "HIGH") __debugbreak();
+}
+#pragma endregion
 
 static void GLClearError()
 {
@@ -19,7 +145,7 @@ static void GLClearError()
 static bool GLLogCall()
 {
 	bool temp = true;
-	while (GLenum error = glGetError() )
+	while (GLenum error = glGetError())
 	{
 		std::cout << "[OpenGL Error] " << error << std::endl;
 		temp = false;
@@ -109,7 +235,9 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 int main(void)
 {
 	GLFWwindow* window;
-
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
@@ -126,11 +254,33 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 
 	/* Fetch the necessary driver calls, needs a valid OpenGL context */
 	if (glewInit() != GLEW_OK) std::cout << "Error glewInit" << std::endl;
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
+
+#pragma region INIT DEBUG OUTPUT
+	// =============== INIT DEBUG OUTPUT ================
+	// The following function calls should be made directly after OpenGL
+	// initialization.
+
+	// Enable the debugging layer of OpenGL
+	//
+	// GL_DEBUG_OUTPUT - Faster version but not useful for breakpoints
+	// GL_DEBUG_OUTPUT_SYNCHRONUS - Callback is in sync with errors, so a breakpoint
+	// can be placed on the callback in order to get a stacktrace for the GL error.
+
+	glEnable(GL_DEBUG_OUTPUT);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+
+	// Set the function that will be triggered by the callback, the second parameter
+	// is the data parameter of the callback, it can be useful for different
+	// contexts but isn't necessary for our simple use case.
+	glDebugMessageCallback(GLDebugMessageCallback, NULL);
+#pragma endregion
+
 
 	float positions[] = {
 		-0.5f, -0.5f,
@@ -144,10 +294,14 @@ int main(void)
 		2,3,0
 	};
 
+	unsigned int vao;
+	glGenVertexArrays(1,&vao);
+	glBindVertexArray(vao);
+
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 2 * 6 * sizeof(float), positions, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
@@ -157,7 +311,6 @@ int main(void)
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-
 	std::string shaderPath = "./res/shaders/basic.shader";
 	ShaderProgramSource source = ParseShader(shaderPath);
 	std::cout << "VERTEX" << std::endl;
@@ -166,14 +319,32 @@ int main(void)
 	std::cout << source.FragmentSource << std::endl;
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
+
+	clock_t timer = clock();
+	int location = glGetUniformLocation(shader, "u_Color");
+	ASSERT(location != -1);
+	glUniform4f(location, timer / CLOCKS_PER_SEC, 1 - (timer / CLOCKS_PER_SEC), 0, 1);
+
+	glUseProgram(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUseProgram(shader);
+
+		timer = clock();
+		glUniform4f(location, (double)(timer % CLOCKS_PER_SEC) / (double)CLOCKS_PER_SEC, 1.0 - ((double)(timer % CLOCKS_PER_SEC) / (double)CLOCKS_PER_SEC), 0.0, 1.0);
+
+		glBindBuffer(GL_ARRAY_BUFFER, buffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-		
+
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
